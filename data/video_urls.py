e4297c380482413daad252a3969759c086de40f0
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-11-04 17:21:58 +0530

    Eypz

---
+++ data/video_urls.py
@@ -0,0 +1,10 @@
+eypz = [
+    "https://files.catbox.moe/nkd0ji.mp4",
+    "https://files.catbox.moe/6hcsyi.mp4",
+    "https://files.catbox.moe/eixy1n.mp4",
+    "https://files.catbox.moe/rhujzu.mp4",
+    "https://files.catbox.moe/01i93a.mp4",
+    "https://files.catbox.moe/8hqdls.mp4",
+    "https://files.catbox.moe/wh5vpz.mp4",
+    "https://files.catbox.moe/5cywgy.mp4"
+]

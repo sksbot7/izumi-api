1e9e75ee0189c052afc2aaa79086a997ccc9402e
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-11-04 17:21:20 +0530

    Eypz

---
+++ data/image_urls.py
@@ -0,0 +1,7 @@
+izumi = [
+    "https://files.catbox.moe/otolcf.png",
+    "https://files.catbox.moe/zpn5zi.png",
+    "https://files.catbox.moe/sqi2e2.png",
+    "https://files.catbox.moe/1yhcut.png",
+    "https://files.catbox.moe/djyj90.png"
+]
